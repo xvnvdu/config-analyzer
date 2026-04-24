c91b36cf71c75a9baabe1b3759cd22c0f7214883
Ivan <vanzag1738@ya.ru>
2026-04-25 00:19:47 +0300

    feat: add config parser

---
+++ internal/parser/parser.go
@@ -0,0 +1,42 @@
+package parser
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/xvnvdu/config-analyzer/internal/domain"
+	"gopkg.in/yaml.v3"
+)
+
+func ParseFile(path string) (domain.Config, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	return parse(data)
+}
+
+func ParseStdin() (domain.Config, error) {
+	data, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		return nil, err
+	}
+	return parse(data)
+}
+
+func parse(data []byte) (domain.Config, error) {
+	var cfg domain.Config
+
+	jsonErr := json.Unmarshal(data, &cfg)
+	if jsonErr == nil {
+		return cfg, nil
+	}
+	yamlErr := yaml.Unmarshal(data, &cfg)
+	if yamlErr == nil {
+		return cfg, nil
+	}
+
+	return nil, fmt.Errorf("failed to parse config: json: %v, yaml: %v", jsonErr, yamlErr)
+}
